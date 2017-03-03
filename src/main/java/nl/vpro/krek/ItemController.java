11dd830d43e0152e2f278c2f3a2ffd877755c92c
Nils Breunese <nils@breun.nl>
2017-03-03 13:16:53 +0100

    Filter als object naar itempagina.

--- src/main/java/nl/vpro/krek/ItemController.java
+++ src/main/java/nl/vpro/krek/ItemController.java
@@ -16,19 +16,14 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @GetMapping(value = "/item/{id}")
+    @GetMapping(value = "/items/{id}")
     public String index(@PathVariable("id") final Long id, Model model) {
         Item item = itemService.getById(id).orElseThrow(() -> new IllegalArgumentException("No item for id " + id));
         Filter filter = item.getFilter();
         model.addAttribute("titel", item.getTitle());
         model.addAttribute("auteur", item.getAuthor());
         model.addAttribute("tekst", item.getText());
-        model.addAttribute("filter_naam", filter.getName());
-        model.addAttribute("filter_kleur", toHex(filter.getColor()));
+        model.addAttribute("filter", filter);
         return "item";
-    }
-
-    private String toHex(final Color color) {
-        return String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
     }
 }
